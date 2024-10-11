7d284e8500092590d06346b5a5cd2308c86c5879
Carlos Sánchez Páez <karlossanpa@gmail.com>
2024-10-11 07:58:44 +0200

    Add healthcheck

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.infrastructure.routers.mercadona import router as mercadona_router
+from src.infrastructure.routers.health import router as health_router
 
 app = FastAPI()
 
+app.include_router(health_router, prefix="/health")
 app.include_router(mercadona_router, prefix="/mercadona")
 
 if __name__ == "__main__":
